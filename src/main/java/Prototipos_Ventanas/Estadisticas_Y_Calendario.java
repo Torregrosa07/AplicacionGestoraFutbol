57023b40ab16ddce15867c6d50970720004ba392
keinyhm <keinyhm@gmail.com>
2025-04-01 13:57:37 +0200

    Prueba Imagen Ventana

--- src/main/java/Prototipos_Ventanas/Estadisticas_Y_Calendario.java
+++ src/main/java/Prototipos_Ventanas/Estadisticas_Y_Calendario.java
@@ -32,11 +32,11 @@ public class Estadisticas_Y_Calendario extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 850, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 545, Short.MAX_VALUE)
         );
 
         pack();
