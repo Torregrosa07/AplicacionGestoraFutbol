2e5a41ca28c9e6ea9d55fc37cc8030e2b389c14e
Santiago Torregrosa <santiago.torre07a@gmail.com>
2025-04-01 09:51:55 +0200

    Santiago

---
+++ src/main/java/equipo5/com/kickoff_tournaments_aplicacion_gestora_futbol/KickOff_Tournaments_Aplicacion_Gestora_Futbol.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package equipo5.com.kickoff_tournaments_aplicacion_gestora_futbol;
+
+/**
+ *
+ * @author Santiago
+ */
+public class KickOff_Tournaments_Aplicacion_Gestora_Futbol {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Santiago");
+        
+    }
+}
