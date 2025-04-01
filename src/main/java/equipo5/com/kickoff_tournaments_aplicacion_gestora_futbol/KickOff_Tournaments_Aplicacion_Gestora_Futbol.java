f6f4a771fee829f9065c493190011aa2383542b4
keinyhm <keinyhm@gmail.com>
2025-04-01 11:08:26 +0200

    Prueba

--- src/main/java/equipo5/com/kickoff_tournaments_aplicacion_gestora_futbol/KickOff_Tournaments_Aplicacion_Gestora_Futbol.java
+++ src/main/java/equipo5/com/kickoff_tournaments_aplicacion_gestora_futbol/KickOff_Tournaments_Aplicacion_Gestora_Futbol.java
@@ -16,7 +16,7 @@ public class KickOff_Tournaments_Aplicacion_Gestora_Futbol {
         System.out.println("Keiny");
         System.out.println("Thomas12321");
         System.out.println("    When estas asi bien maniacote");
-        
+        System.out.println("Hola ola ola");
     
         
     }
