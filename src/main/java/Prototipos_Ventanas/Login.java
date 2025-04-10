0219ea3bdcc01e3347e577baf6590dfa316afcfb
Thomasquira <thomasquirogudelo@hotmail.com>
2025-04-10 09:22:02 +0200

    PruebaBase

--- src/main/java/Prototipos_Ventanas/Login.java
+++ src/main/java/Prototipos_Ventanas/Login.java
@@ -238,10 +238,7 @@ public class Login extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-
-      
-            controladorUsuarios.crearAdminSiNoExiste();
-
+       
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         try {
