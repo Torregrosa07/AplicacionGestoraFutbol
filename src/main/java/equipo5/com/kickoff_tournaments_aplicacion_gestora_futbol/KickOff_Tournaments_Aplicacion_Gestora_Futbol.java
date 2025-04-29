abcaa06701cd87b87b83091a41dda533ef340374
Thomasquira <thomasquirogudelo@hotmail.com>
2025-04-29 20:10:36 +0200

    Arreglo instancia

--- src/main/java/equipo5/com/kickoff_tournaments_aplicacion_gestora_futbol/KickOff_Tournaments_Aplicacion_Gestora_Futbol.java
+++ src/main/java/equipo5/com/kickoff_tournaments_aplicacion_gestora_futbol/KickOff_Tournaments_Aplicacion_Gestora_Futbol.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package equipo5.com.kickoff_tournaments_aplicacion_gestora_futbol;
 
 import Prototipos_Ventanas.Login;
@@ -14,7 +13,8 @@ public class KickOff_Tournaments_Aplicacion_Gestora_Futbol {
 
     public static void main(String[] args) {
         Login login = new Login();
-    
-        
+        login.setLocationRelativeTo(null); // Centra la ventana en la pantalla
+        login.setVisible(true);            // ¡Importante! Muestra la ventana
     }
+
 }
